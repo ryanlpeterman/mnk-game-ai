90e0a948c8d0a81976f0ed42674537d13a3d2b2c
Scott Shi <scottyshi@gmail.com>
2016-06-25 14:36:38 -0400

    set up the environement

--- board.py
+++ board.py
@@ -4,4 +4,20 @@ class Board:
 	DIMENSION = 3
 
 	def __init__(self):
-		# self.board =
+		#represented as integer matrix
+		#'-1' is blank
+		#'0' is player 1's tick
+		#'1' is player 2's tick
+		self.board = np.fill((3,3),-1);#empty board
+	
+	def display(self):
+		"""display the entire grid"""
+		print "TODO"
+
+	def checkMove(self, x, y):
+		"""verify if it's a correct move"""
+		print "TODO"
+
+	def setMove(self, x, y, tick):
+		"""sets a valid move on the grid"""
+		print "TODO"
