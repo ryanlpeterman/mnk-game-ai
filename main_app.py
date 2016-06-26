b2791d27399e4ecc562fb680a4b697bb9ea43ba1
Ryan Peterman <ryanlpeterman@gmail.com>
2016-06-26 14:58:14 -0400

    change port

--- main_app.py
+++ main_app.py
@@ -8,5 +8,5 @@ def hello():
     return "Hello world!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port)
