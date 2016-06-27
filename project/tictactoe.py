2e24a64ac88bb8e94bc8d9330ccbe1dcb6f2005e
Ryan Peterman <ryanlpeterman@gmail.com>
2016-06-26 22:44:39 -0400

    add improved cli

--- project/tictactoe.py
+++ project/tictactoe.py
@@ -3,24 +3,32 @@
 
 import player
 import board
+import argparse
 
-def game():
+def game(player1_type, player2_type, num_games=1):
     """
     Game loop for tictactoe
     """
-    dumbcnt = 0
-    smartcnt = 0
-    for _ in range(0, 5):
-        brd = board.Board()
-        player2 = player.PerfectAIPlayer(0)
-        player1 = player.RandomAIPlayer(1)
+    player1_type += 'Player'
+    player2_type += 'Player'
 
+    # get constructor of player based on command line argument
+    player1 = getattr(player, player1_type)(1)
+    player2 = getattr(player, player2_type)(0)
+
+    p1_win_cnt = 0
+    p2_win_cnt = 0
+
+    for _ in range(0, num_games):
+
+        brd = board.Board()
+
+        # ref to current player
         curr_player = player1
 
         while brd.isOver() == -1:
             print "Player " + str(curr_player.marker) + "'s move...",
             brd.display()
-
             row, col = curr_player.make_move(brd)
 
             while not brd.checkMove(row, col):
@@ -34,19 +42,41 @@ def game():
             else:
                 curr_player = player1
 
+        print "Game Over State:",
         brd.display()
-        print "Game Over!"
+
         if brd.isOver() == 2:
             print "Cat's Game"
         elif brd.isOver() == 1:
-            print "Player O Wins!!"
-            dumbcnt += 1
+            print player1_type + " (player1) Wins!!"
+            p1_win_cnt += 1
         elif brd.isOver() == 0:
-            print "Player X Wins!!"
-            smartcnt += 1
+            print player2_type + " (player2) Wins!!"
+            p2_win_cnt += 1
 
-    print "Smart AI Won " + str(smartcnt) + " Times!"
-    print "Random AI Won " + str(dumbcnt) + " Times!"
+    print "\nSummary of Games Played:"
+    print player1_type + " (player1) won " + str(p1_win_cnt) + " Times!"
+    print player2_type + " (player2) won " + str(p2_win_cnt) + " Times!"
+    print "There were " + str(num_games - (p1_win_cnt + p2_win_cnt)) + " Cat's Games"
 
 if __name__ == '__main__':
-    game()
+
+    parser = argparse.ArgumentParser()
+
+    # optional arguments with associated default values
+    parser.add_argument("-p1", "--player1", nargs='?', const="Human", default="Human",
+                help="specify \"Human\", \"PerfectAI\", or \"RandomAI\" for p1")
+    parser.add_argument("-p2", "--player2", nargs='?', const="PerfectAI", default="PerfectAI",
+                help="specify \"Human\", \"PerfectAI\", or \"RandomAI\" for p2")
+    parser.add_argument("-n", "--num", nargs='?', const=1, type=int, default=1,
+                help="num games to be played, defaults to 1")
+
+    args = parser.parse_args()
+    valid_inputs = ["Human", "PerfectAI", "RandomAI"]
+
+    # check if player inputs are correct
+    if not args.player1 in valid_inputs or not args.player2 in valid_inputs:
+        print "Player parameters must either be \"Human\", \"PerfectAI\", or \"RandomAI\""
+        exit()
+
+    game(args.player1, args.player2, args.num)
