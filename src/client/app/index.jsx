09d1af1d488e708a052114cae2538cb35c344bc1
Ryan Peterman <ryanlpeterman@gmail.com>
2016-07-23 22:19:16 -0400

    refactor and update board

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Board from './components/Board';
 
-
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 class App extends React.Component {
   render () {
-    return <Board/>;
+    return (
+    	<MuiThemeProvider muiTheme={getMuiTheme()}>
+    		<Board/>
+		</MuiThemeProvider>
+    );
   }
 }
 
